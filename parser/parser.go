dfd7e2aa4931aab4a1d265890562af9edc937f31
Marcus <marcus@cigotracker.com>
2024-05-01 19:20:10 -0300

    wip identifiers

--- parser/parser.go
+++ parser/parser.go
@@ -12,6 +12,10 @@ type Parser struct {
 	curToken  token.Token
 	peekToken token.Token
 	errors    []string
+
+	prefixParseFns  map[string]prefixParseFn
+	infixParseFns   map[string]infixParseFn
+	parseExpression func(int) ast.Expression
 }
 
 func New(l *lexer.Lexer) *Parser {
@@ -59,7 +63,7 @@ func (p *Parser) parseStatement() ast.Statement {
 	case string(token.RETURN):
 		return p.parseReturnStatement()
 	default:
-		return nil
+		return p.parseExpressionStatement()
 	}
 }
 
@@ -105,3 +109,36 @@ func (p *Parser) expectPeek(t token.TokenType) bool {
 		return false
 	}
 }
+
+type (
+	prefixParseFn func() ast.Expression
+	infixParseFn  func(ast.Expression) ast.Expression
+)
+
+func (p *Parser) registerPrefix(tokenType token.TokenType, fn prefixParseFn) {
+	p.prefixParseFns[string(tokenType)] = fn
+}
+func (p *Parser) registerInfix(tokenType token.TokenType, fn infixParseFn) {
+	p.infixParseFns[string(tokenType)] = fn
+}
+
+func (p *Parser) parseExpressionStatement() *ast.ExpressionStatement {
+	stmt := &ast.ExpressionStatement{Token: p.curToken}
+	stmt.Expression = p.parseExpression(LOWEST)
+	if p.peekTokenIs(token.SEMICOLON) {
+		p.nextToken()
+	}
+	return stmt
+}
+
+const (
+	_ int = iota
+	LOWEST
+	EQUALS
+	// ==
+	LESSGREATER // > or <
+	SUM         // +
+	PRODUCT     // *
+	PREFIX      // -X or !X
+	CALL        // myFunction(X)
+)
