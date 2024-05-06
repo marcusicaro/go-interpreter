74460cc67210fbf285ee3becd0974e8d310444bb
Marcus <marcus@cigotracker.com>
2024-05-06 20:58:23 -0300

    implement boolean on parser

--- ast/ast.go
+++ ast/ast.go
@@ -146,3 +146,12 @@ func (oe *InfixExpression) String() string {
 	out.WriteString(")")
 	return out.String()
 }
+
+type Boolean struct {
+	Token token.Token
+	Value bool
+}
+
+func (b *Boolean) expressionNode()      {}
+func (b *Boolean) TokenLiteral() []rune { return b.Token.Literal }
+func (b *Boolean) String() string       { return string(b.Token.Literal) }
