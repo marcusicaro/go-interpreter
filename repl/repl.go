5f6bbb10a0b56fce82ae6a0dc28708fc4d718794
Marcus <marcus@cigotracker.com>
2024-05-14 19:48:41 -0300

    implement evaluator test

--- repl/repl.go
+++ repl/repl.go
@@ -5,7 +5,7 @@ import (
 	"fmt"
 	"io"
 	"monkey/lexer"
-	"monkey/token"
+	"monkey/parser"
 )
 
 const PROMPT = ">> "
@@ -18,10 +18,26 @@ func Start(in io.Reader, out io.Writer) {
 		if !scanned {
 			return
 		}
-		line := []rune(scanner.Text())
-		l := lexer.New(line)
-		for tok := l.NextToken(); string(tok.Type) != string(token.EOF); tok = l.NextToken() {
-			fmt.Printf("%+v\n", tok)
+		line := scanner.Text()
+		l := lexer.New([]rune(line))
+		p := parser.New(l)
+		program := p.ParseProgram()
+		if len(p.Errors()) != 0 {
+			printParserErrors(out, p.Errors())
+			continue
 		}
+		io.WriteString(out, program.String())
+		io.WriteString(out, "\n")
+	}
+}
+
+const MONKEY_FACE = `====`
+
+func printParserErrors(out io.Writer, errors []string) {
+	io.WriteString(out, MONKEY_FACE)
+	io.WriteString(out, "Woops! We ran into some monkey business here!\n")
+	io.WriteString(out, " parser errors:\n")
+	for _, msg := range errors {
+		io.WriteString(out, "\t"+msg+"\n")
 	}
 }
