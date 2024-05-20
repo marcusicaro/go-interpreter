0d90f5605ec1fed77f591e9286295da2253e9fd4
marcusicaro <marcusicaromc@gmail.com>
2024-05-20 19:42:20 -0300

    feat: implement if/else evaluation

--- evaluator/evaluator.go
+++ evaluator/evaluator.go
@@ -7,9 +7,7 @@ import (
 
 func Eval(node ast.Node) object.Object {
 	switch node := node.(type) {
-	// Statements
-	case *ast.Program:
-		return evalStatements(node.Statements)
+
 	case *ast.ExpressionStatement:
 		return Eval(node.Expression)
 	// Expressions
@@ -25,14 +23,27 @@ func Eval(node ast.Node) object.Object {
 		right := Eval(node.Right)
 		return evalInfixExpression(node.Operator, left, right)
 	case *ast.BlockStatement:
-		return evalStatements(node.Statements)
+		return evalBlockStatement(node)
 	case *ast.IfExpression:
 		return evalIfExpression(node)
 	case *ast.ReturnStatement:
 		val := Eval(node.ReturnValue)
 		return &object.ReturnValue{Value: val}
+	case *ast.Program:
+		return evalProgram(node)
 	}
 	return nil
+}
+
+func evalBlockStatement(block *ast.BlockStatement) object.Object {
+	var result object.Object
+	for _, statement := range block.Statements {
+		result = Eval(statement)
+		if result != nil && result.Type() == object.RETURN_VALUE_OBJ {
+			return result
+		}
+	}
+	return result
 }
 
 func evalIfExpression(ie *ast.IfExpression) object.Object {
@@ -135,9 +146,9 @@ func evalBangOperatorExpression(right object.Object) object.Object {
 	}
 }
 
-func evalStatements(stmts []ast.Statement) object.Object {
+func evalProgram(program *ast.Program) object.Object {
 	var result object.Object
-	for _, statement := range stmts {
+	for _, statement := range program.Statements {
 		result = Eval(statement)
 		if returnValue, ok := result.(*object.ReturnValue); ok {
 			return returnValue.Value
