f5bedab4f7e61c93789e3262ba47b4dd8faa9fbb
Marcus <marcusicaromc@gmail.com>
2024-11-20 21:09:56 -0300

    implement array index and array methods

--- object/object.go
+++ object/object.go
@@ -87,11 +87,29 @@ const (
 	ERROR_OBJ        = "ERROR"
 	FUNCTION_OBJ     = "FUNCTION"
 	STRING_OBJ       = "STRING"
-	BUILTIN_OBJ = "BUILTIN"
+	BUILTIN_OBJ      = "BUILTIN"
+	ARRAY_OBJ        = "ARRAY"
 )
+
+type Array struct {
+	Elements []Object
+}
 
 type Builtin struct {
 	Fn BuiltinFunction
+}
+
+func (b *Builtin) Type() ObjectType { return BUILTIN_OBJ }
+func (b *Builtin) Inspect() string  { return "builtin function" }
+func (ao *Array) Type() ObjectType  { return ARRAY_OBJ }
+func (ao *Array) Inspect() string {
+	var out bytes.Buffer
+	elements := []string{}
+	for _, e := range ao.Elements {
+		elements = append(elements, e.Inspect())
 	}
-	func (b *Builtin) Type() ObjectType { return BUILTIN_OBJ }
-	func (b *Builtin) Inspect() string { return "builtin function" }
+	out.WriteString("[")
+	out.WriteString(strings.Join(elements, ", "))
+	out.WriteString("]")
+	return out.String()
+}
