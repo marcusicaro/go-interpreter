31850e5bd58493b6a2198f3e6f7a350b9b469087
Marcus <marcusicaromc@gmail.com>
2024-11-26 18:29:15 -0300

    implement map function

--- evaluator/builtins.go
+++ evaluator/builtins.go
@@ -4,137 +4,74 @@ import (
 	"monkey/object"
 )
 
-var builtins = map[string]*object.Builtin{
-	"len": &object.Builtin{
-		Fn: func(args ...object.Object) object.Object {
-			if len(args) != 1 {
-				return newError("wrong number of arguments. got=%d, want=1",
-					len(args))
-			}
-			switch arg := args[0].(type) {
-			case *object.Array:
-				return &object.Integer{Value: int64(len(arg.Elements))}
-			case *object.String:
-				return &object.Integer{Value: int64(len(arg.Value))}
-			default:
-				return newError("argument to `len` not supported, got %s",
-					args[0].Type())
-			}
-		},
-	},
-	"first": &object.Builtin{
-		Fn: func(args ...object.Object) object.Object {
-			if len(args) != 1 {
-				return newError("wrong number of arguments. got=%d, want=1",
-					len(args))
-			}
-			if args[0].Type() != object.ARRAY_OBJ {
-				return newError("argument to `first` must be ARRAY, got %s",
-					args[0].Type())
-			}
-			arr := args[0].(*object.Array)
-			if len(arr.Elements) > 0 {
-				return arr.Elements[0]
-			}
-			return NULL
-		},
-	},
-	"last": &object.Builtin{
-		Fn: func(args ...object.Object) object.Object {
-			if len(args) != 1 {
-				return newError("wrong number of arguments. got=%d, want=1",
-					len(args))
-			}
-			if args[0].Type() != object.ARRAY_OBJ {
-				return newError("argument to `last` must be ARRAY, got %s",
-					args[0].Type())
-			}
-			arr := args[0].(*object.Array)
-			if len(arr.Elements) > 0 {
-				return arr.Elements[len(arr.Elements)-1]
-			}
-			return NULL
-		},
-	},
-	"rest": &object.Builtin{
-		Fn: func(args ...object.Object) object.Object {
-			if len(args) != 1 {
-				return newError("wrong number of arguments. got=%d, want=1",
-					len(args))
-			}
-			if args[0].Type() != object.ARRAY_OBJ {
-				return newError("argument to `rest` must be ARRAY, got %s",
-					args[0].Type())
-			}
-			arr := args[0].(*object.Array)
-			length := len(arr.Elements)
-			if length > 0 {
-				newElements := make([]object.Object, length-1, length-1)
-				copy(newElements, arr.Elements[1:length])
-				return &object.Array{Elements: newElements}
-			}
-			return NULL
-		},
-	},
-	"push": &object.Builtin{
-		Fn: func(args ...object.Object) object.Object {
-			if len(args) != 2 {
-				return newError("wrong number of arguments. got=%d, want=2",
-					len(args))
-			}
-			if args[0].Type() != object.ARRAY_OBJ {
-				return newError("argument to `push` must be ARRAY, got %s",
-					args[0].Type())
-			}
-			arr := args[0].(*object.Array)
-			length := len(arr.Elements)
-			newElements := make([]object.Object, length+1, length+1)
-			copy(newElements, arr.Elements)
-			newElements[length] = args[1]
-			return &object.Array{Elements: newElements}
-		},
-	},
-	"contains": &object.Builtin{
-		Fn: func(args ...object.Object) object.Object {
-			if len(args) != 2 {
-				return newError("wrong number of arguments. got=%d, want=2",
-					len(args))
-			}
-			if args[0].Type() != object.ARRAY_OBJ {
-				return newError("argument to `rest` must be ARRAY, got %s",
-					args[0].Type())
-			}
-			arr := args[0].(*object.Array)
-			target := args[1]
+var builtins map[string]*object.Builtin
 
-			for _, elem := range arr.Elements {
-				if elem.Inspect() == target.Inspect() {
-					return &object.Boolean{Value: true}
+func init() {
+	initializeBuiltins()
+}
+
+func initializeBuiltins() {
+	builtins = map[string]*object.Builtin{
+		"len": &object.Builtin{
+			Fn: func(args ...object.Object) object.Object {
+				if len(args) != 1 {
+					return newError("wrong number of arguments. got=%d, want=1", len(args))
 				}
-			}
-			return &object.Boolean{Value: false}
+				switch arg := args[0].(type) {
+				case *object.Array:
+					return &object.Integer{Value: int64(len(arg.Elements))}
+				case *object.String:
+					return &object.Integer{Value: int64(len(arg.Value))}
+				default:
+					return newError("argument to `len` not supported, got %s", args[0].Type())
+				}
+			},
 		},
-	},
-	"map": &object.Builtin{
-		Fn: func(args ...object.Object) object.Object {
-			if len(args) != 2 {
-				return newError("wrong number of arguments. got=%d, want=2",
-					len(args))
-			}
-			if args[0].Type() != object.ARRAY_OBJ {
-				return newError("argument to `rest` must be ARRAY, got %s",
-					args[0].Type())
-			}
+		"contains": &object.Builtin{
+			Fn: func(args ...object.Object) object.Object {
+				if len(args) != 2 {
+					return newError("wrong number of arguments. got=%d, want=2", len(args))
+				}
+				if args[0].Type() != object.ARRAY_OBJ {
+					return newError("argument to `contains` must be ARRAY, got %s", args[0].Type())
+				}
+				arr := args[0].(*object.Array)
+				target := args[1]
 
-			arr := args[0].(*object.Array)
-			fn := args[1].(*object.Function)
-
-			var newElements []object.Object
-			for _, elem := range arr.Elements {
-				evaluated := applyFunction(fn, []object.Object{elem})
-
-			}
-
+				for _, elem := range arr.Elements {
+					if elem.Inspect() == target.Inspect() {
+						return &object.Boolean{Value: true}
+					}
+				}
+				return &object.Boolean{Value: false}
+			},
 		},
-	},
+		"map": &object.Builtin{
+			Fn: func(args ...object.Object) object.Object {
+				if len(args) != 2 {
+					return newError("wrong number of arguments. got=%d, want=2", len(args))
+				}
+				if args[0].Type() != object.ARRAY_OBJ {
+					return newError("argument to `map` must be ARRAY, got %s", args[0].Type())
+				}
+				if args[1].Type() != object.FUNCTION_OBJ {
+					return newError("second argument to `map` must be FUNCTION, got %s", args[1].Type())
+				}
+
+				arr := args[0].(*object.Array)
+				fn := args[1].(*object.Function)
+
+				var newElements []object.Object
+				for _, elem := range arr.Elements {
+					result := applyFunction(fn, []object.Object{elem})
+					if isError(result) {
+						return result
+					}
+					newElements = append(newElements, result)
+				}
+
+				return &object.Array{Elements: newElements}
+			},
+		},
+	}
 }
