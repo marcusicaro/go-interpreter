afdfb989b058412dfd204cafe00c56e3ac9334f6
Marcus <marcusicaromc@gmail.com>
2024-11-27 20:15:22 -0300

    add reduce and indexOf builtin functions for arrays

--- evaluator/builtins.go
+++ evaluator/builtins.go
@@ -64,13 +64,61 @@ func initializeBuiltins() {
 				var newElements []object.Object
 				for _, elem := range arr.Elements {
 					result := applyFunction(fn, []object.Object{elem})
-					if isError(result) {
-						return result
-					}
 					newElements = append(newElements, result)
 				}
 
 				return &object.Array{Elements: newElements}
+			},
+		},
+		"reduce": &object.Builtin{
+			Fn: func(args ...object.Object) object.Object {
+				if len(args) != 3 && len(args) != 2 {
+					return newError("wrong number of arguments. got=%d, want=2 or 3", len(args))
+				}
+
+				if args[0].Type() != object.ARRAY_OBJ {
+					return newError("argument to `reduce` must be ARRAY, got %s", args[0].Type())
+				}
+
+				if args[1].Type() != object.FUNCTION_OBJ {
+					return newError("argument to `reduce` must be ARRAY, got %s", args[0].Type())
+				}
+
+				arr := args[0].(*object.Array)
+				fn := args[1].(*object.Function)
+
+				var acumulator object.Object = nil
+				if len(args) == 3 {
+					acumulator = args[2]
+				} else {
+					acumulator = arr.Elements[0]
+				}
+
+				for _, elem := range arr.Elements {
+					acumulator = applyFunction(fn, []object.Object{acumulator, elem})
+				}
+
+				return acumulator
+			},
+		},
+
+		"indexOf": &object.Builtin{
+			Fn: func(args ...object.Object) object.Object {
+				if len(args) != 2 {
+					return newError("wrong number of arguments. got=%d, want=2", len(args))
+				}
+				if args[0].Type() != object.ARRAY_OBJ {
+					return newError("argument to `indexOf` must be ARRAY, got %s", args[0].Type())
+				}
+				arr := args[0].(*object.Array)
+				target := args[1]
+
+				for i, elem := range arr.Elements {
+					if elem.Inspect() == target.Inspect() {
+						return &object.Integer{Value: int64(i)}
+					}
+				}
+				return &object.Integer{Value: -1}
 			},
 		},
 	}
